4e181dfdd79197fadd44e10a4e6b6e40f625bb15
Danny Colombara <dcolombara@kingcounty.gov>
2020-02-05 14:43:57 -0800

    Changed from copying stage >> final to using ALTER SCHEMA
     - faster / cleaner

--- ETL/birth/final/load_final.birth_wa.sql
+++ ETL/birth/final/load_final.birth_wa.sql
@@ -1,28 +1,10 @@
-/****** COPY FROM STAGE to FINAL  ******/
-	IF OBJECT_ID('[PH_APDEStore].[final].[bir_wa]', 'U') IS NOT NULL 
-		DROP TABLE [PH_APDEStore].[final].[bir_wa]
-
-	SELECT *
-		INTO [PH_APDEStore].[final].[bir_wa]	
-		FROM [PH_APDEStore].[stage].[bir_wa]
-
+/****** CHANGE SCHEMA FROM STAGE to FINAL  ******/
+DROP TABLE [PH_APDEStore].[final].[bir_wa]
+ALTER SCHEMA [final] 
+    TRANSFER [stage].[bir_wa]
 
 /****** ADD COLUMSTORE CLUSTERED INDEX ******/
 	CREATE CLUSTERED COLUMNSTORE INDEX idx_final_bir_wa
 	ON [PH_APDEStore].[final].[bir_wa]
 	WITH (DROP_EXISTING = OFF)
 
-
-/****** BASIC ERROR CHECKING COMPARING STAGE & FINAL ******/
-	SELECT COUNT(*) FROM [PH_APDEStore].[stage].[bir_wa]
-	SELECT COUNT(*) FROM [PH_APDEStore].[final].[bir_wa]
-
-	SELECT kotelchuck, 
-	count(*) FROM [PH_APDEStore].[stage].[bir_wa]
-	  GROUP BY kotelchuck
-	  ORDER BY -count(*)
-
-	 SELECT kotelchuck, 
-	count(*) FROM [PH_APDEStore].[final].[bir_wa]
-	  GROUP BY kotelchuck
-	  ORDER BY -count(*)
