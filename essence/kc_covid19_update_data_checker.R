49c706933c853639aefa6dff71edcc33144e15b4
Alastair Matheson <alastair.matheson@gmail.com>
2020-04-20 13:51:47 -0700

    Daily check on ESSENCE data refresh sends an email regardless of success of failure

--- essence/kc_covid19_update_data_checker.R
+++ essence/kc_covid19_update_data_checker.R
@@ -2,7 +2,7 @@
 ##
 ## Script name: kc_covid19_update_data_checker
 ##
-## Purpose of script: Alert people if the syndromic surveillance report failed
+## Purpose of script: Alert people if the syndromic surveillance report completed
 ##
 ## Author: Alastair Matheson, Public Health - Seattle & King County
 ## Date Created: 2020-04-19
@@ -24,11 +24,15 @@ pacman::p_load(tidyverse, glue, RDCOMClient, lubridate)
 ### Set up folder to check
 output_path <- "//phshare01/cdi_share/Analytics and Informatics Team/Data Requests/2020/372_nCoV Essence Extract/From Natasha on March 13"
 
+
+#### CHECK FILES ####
 ### See when the daily file was last updated
 last_mod <- file.mtime(file.path(output_path, "ndly.csv"))
 
 # Also check weekly file if today is Monday
 if (wday(today(), label = F, week_start = getOption("lubridate.week.start", 1)) == 1) {
+  monday <- T
+  
   last_mod_wkly <- file.mtime(file.path(output_path, "nwkly.csv"))
   
   if (date(last_mod_wkly) != today()) {
@@ -37,45 +41,63 @@ if (wday(today(), label = F, week_start = getOption("lubridate.week.start", 1)) 
     weekly_error <- F
   }
 } else {
+  monday <- F
   weekly_error <- F
 }
 
-if (date(last_mod) != today() & weekly_error == F) {
-  error <- T
-  error_text <- "the daily syndromic data file (ndly.csv) was not updated today."
-} else if (date(last_mod) != today() & weekly_error == T) {
-  error <- T
-  error_text <- "both the daily (ndly.csv) and weekly (nwkly.csv) syndromic data files were not updated today."
-} else if (date(last_mod) == today() & weekly_error == T) {
-  error <- T
-  error_text <- "the weekly (nwkly.csv) syndromic data file was not updated today (but the daily file (ndly.csv) was)."
+if (date(last_mod) != today()) {
+  daily_error <- T
 } else {
-  error <- F
-  error_text <- ""
+  daily_error <- F
 }
 
 
-if (error == F) {
-  # Set up COM API
-  outlook_app <- COMCreate("Outlook.Application")
-  # Create an email
-  outlook_mail <- outlook_app$CreateItem(0)
-  # Configure email
-  outlook_mail[["To"]] <- "alastair.matheson@kingcounty.gov; jlenahan@kingcounty.gov"
-  outlook_mail[["subject"]] <- "Daily syndromic report failed to run"
-  additional_msg <- ""
-  outlook_mail[["htmlbody"]] <- paste0(
-    "<p>This is an automatically generated email to let you know that ", error_text,  "</p>",
-    "<p>As a reminder, the daily file should be updated shortly after 4 PM each day and the weekly file each Monday at 4 PM. ", 
-    "This email is generated if either file's last modified date is not as expected. </p>",
-    "<p>Check <a href = 'file:\\\\Phshare01\\cdi_share\\Analytics and Informatics Team\\Data Requests\\2020\\372_nCoV Essence Extract\\From Natasha on March 13'>
+#### GENERATE TEXT ####
+if (daily_error == T & monday == F) {
+  error_text <- "the daily syndromic data file (ndly.csv) was not updated today."
+} else if (daily_error == T & monday == T & weekly_error == F) {
+  error_text <- paste0("the daily syndromic data file (ndly.csv) was not updated today ", 
+                       "(and the weekly code likely failed to even start).")
+} else if (daily_error == T & weekly_error == T) {
+  error_text <- "both the daily (ndly.csv) and weekly (nwkly.csv) syndromic data files were not updated today."
+} else if (daily_error == F & weekly_error == T) {
+  error_text <- "the weekly syndromic data file (nwkly.csv) was not updated today (but the daily file (ndly.csv) was)."
+} else if (daily_error == F & monday == F) {
+  error_text <- "the daily syndromic data file (ndly.csv) was updated today"
+} else if (daily_error == F & monday == T & weekly_error == F) {
+  error_text <- "both the daily (ndly.csv) and weekly (wkly.csv) syndromic data files were updated today"
+}
+
+if (daily_error == T | weekly_error == T) {
+  subject <- "Error in daily syndromic data refresh"
+  body_text <- paste0("This email is generated if either file's last modified date is not as expected. </p>",
+  "<p>Check <a href = 'file:\\\\Phshare01\\cdi_share\\Analytics and Informatics Team\\Data Requests\\2020\\372_nCoV Essence Extract\\From Natasha on March 13'>
   \\\\Phshare01\\cdi_share\\Analytics and Informatics Team\\Data Requests\\2020\\372_nCoV Essence Extract\\From Natasha on March 13</a> to see which files were changed. ",
-    "<br>If the pdly-xxx.csv files were modified, the error is likely in the daily script.", 
-    "<br>If the daily file was modified but the weekly one was not, look in the weekly section of the code.</p>"
-  )
-  
-  # Send email
-  outlook_mail$Send()
-  
+  "<br>If the pdly-xxx.csv files were modified, the error is likely in the daily script.", 
+  "<br>If the daily file was modified but the weekly one was not, look in the weekly section of the code.</p>")
+} else {
+  subject <- "Daily syndromic data refresh completed successfully"
+  body_text <- paste0("Go to <a href = 'https://tableau.kingcounty.gov/#/site/DPH_CDIMMS/views/Essence-SummaryofRespiratorySyndromes-internal/Pg1-CLIED?Set%20frequency=Daily'>", 
+                      "https://tableau.kingcounty.gov/#/site/DPH_CDIMMS/views/Essence-SummaryofRespiratorySyndromes-internal/Pg1-CLIED?Set%20frequency=Daily</a>", 
+                      " to see the dashboard, check the data extract also completed, and download a PDF for the daily email")
 }
+
+
+#### SEND EMAIL ####
+# Set up COM API
+outlook_app <- COMCreate("Outlook.Application")
+# Create an email
+outlook_mail <- outlook_app$CreateItem(0)
+# Configure email
+outlook_mail[["To"]] <- "alastair.matheson@kingcounty.gov; jlenahan@kingcounty.gov"
+outlook_mail[["subject"]] <- subject
+additional_msg <- ""
+outlook_mail[["htmlbody"]] <- paste0(
+  "<p>This is an automatically generated email to let you know that ", error_text,  "</p>",
+  "<p>As a reminder, the daily file should be updated shortly after 4 PM each day and the weekly file each Monday at 4 PM. ", 
+  body_text)
+
+# Send email
+outlook_mail$Send()
+
 
