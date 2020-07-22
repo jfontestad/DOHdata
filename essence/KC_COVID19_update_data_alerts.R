398bdb16c571e03350c1d85f5926c37a46d74fb5
JennyLenahan <67119869+JennyLenahan@users.noreply.github.com>
2020-07-22 09:56:25 -0700

    Update KC_COVID19_update_data_alerts.R
    Added pneumo and all to splines portion.

--- essence/KC_COVID19_update_data_alerts.R
+++ essence/KC_COVID19_update_data_alerts.R
@@ -280,7 +280,7 @@ essence_summary_time <- function(df = pdly_full_all_ed_output,
     ## Run splines on daily dataset only
     ## Set up 3-day rolling average for count
     
-    if (condition %in% c("cli","ili") &
+    if (condition %in% c("cli","ili","pneumo","all") &
         cat_text == "all") {
       
       comb2 <- combined %>%
